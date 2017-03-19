b623d78fbdc76d1be2de8b3101f7e8672c1bb785
Michael Chilton <michaelc962@yahoo.com>
2017-03-19 14:11:02 -0400

    Add more descriptive exception handling through middleware

---
+++ lib/flash.rb
@@ -0,0 +1,30 @@
+require 'json'
+
+class Flash
+  attr_reader :now
+
+  def initialize(req)
+    flash_cookie = req.cookies['_rails_lite_app_flash']
+
+    if flash_cookie
+      @now = JSON.parse(flash_cookie)
+    else
+      @now = {}
+    end
+
+    @flash = {}
+  end
+
+  def [](key)
+    @now[key.to_s] || @flash[key.to_s]
+  end
+
+  def []=(key, val)
+    @flash[key.to_s] = val
+  end
+
+
+  def store_flash(res)    
+    res.set_cookie('_rails_lite_app_flash', value: @flash.to_json, path: '/')
+  end
+end
---
+++ lib/show_exceptions.rb
@@ -0,0 +1,59 @@
+require 'erb'
+require 'byebug'
+class ShowExceptions
+  attr_reader :app
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    begin
+      app.call(env)
+    rescue Exception => e
+      render_exception(e)
+    end
+  end
+
+  private
+
+  def render_exception(e)
+    dir = File.dirname(__FILE__)
+    template_path = File.join(dir, 'templates', 'rescue.html.erb')
+    template = File.read(template_path)
+
+    template_response = ERB.new(template).result(binding)
+
+    Rack::Response.new(template_response, '500', {'Content-Type' => 'text/html'}).finish
+  end
+
+  def error_source_file(e)
+    stack_trace_top = e.backtrace.first.split(':')
+    stack_trace_top(e)[0]
+  end
+
+  def stack_trace_top(e)
+    e.backtrace[0].split(':')
+  end
+
+  def extract_formatted_source(e)
+    source_file_name = error_source_file(e)
+    source_line_num = source_line_num(e)
+
+    source_lines = extract_source(source_file_name)
+  end
+
+  def source_line_num(e)
+    stack_trace_top(e)[1].to_i
+  end
+
+  def extract_source(file)
+    source_file = File.open(file, 'r')
+    source_file.readlines
+  end
+
+  def format_source(source_lines, source_line_num)
+    start = [0, source_line_num - 3].max
+    lines = source_lines[start..(start + 5)]    
+    Hash[*(start+1..(lines.count + start)).zip(lines).flatten]
+  end
+end
