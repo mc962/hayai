42e39258e986510934308c6751efda52024a49e6
Michael Chilton <michaelc962@yahoo.com>
2017-03-19 21:04:21 -0400

    Fix regexp for root route

--- controllers/dragons_controller.rb
+++ controllers/dragons_controller.rb
@@ -1,10 +1,14 @@
 require 'rack'
 require_relative '../lib/controller_base'
 require_relative '../lib/router'
+require_relative '../lib/static'
+require_relative '../lib/show_exceptions'
 
 DRAGONS = [
-  { id: 1, name: "Ryuu", picture_url: 'http://orig10.deviantart.net/9231/f/2014/039/c/2/c2d21022ee46616b6257dfd3a330f1b9-d75mzg9.jpg' },
-  { id: 2, name: "Draco", picture_url: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/800x480-Y_Ddraig_Goch.png' }
+  # { id: 1, name: "Ryuu", picture_url: 'http://orig10.deviantart.net/9231/f/2014/039/c/2/c2d21022ee46616b6257dfd3a330f1b9-d75mzg9.jpg' },
+  # { id: 2, name: "Draco", picture_url: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/800x480-Y_Ddraig_Goch.png' }
+  { id: 1, name: "Ryuu", picture_url: '../public/ryuu-pic.jpg' },
+  { id: 2, name: "Draco", picture_url: '../public/draco-pic.png' }
 ]
 
 class DragonsController < ControllerBase
@@ -22,7 +26,7 @@ end
 
 router = Router.new
 router.draw do
-  get Regexp.new("/"), DragonsController, :index
+  get Regexp.new("^/$"), DragonsController, :index
   get Regexp.new("^/dragons$"), DragonsController, :index
   get Regexp.new("^/dragons/(?<dragon_id>\\d+)"), DragonsController, :show
 end
@@ -33,6 +37,12 @@ app = Proc.new do |env|
   router.run(req, res)
   res.finish
 end
+
+app = Rack::Builder.new do
+  use ShowExceptions
+  use Static
+  run app
+end.to_app
 
 Rack::Server.start(
   app: app,
